8c11efa3ad9ab97b30248bb4d876484ddb5d30b8
ThiagoQuetz <160203494+ThiagoQuetz@users.noreply.github.com>
2024-03-06 19:42:53 -0300

    Update scritps.js

--- scritps.js
+++ scritps.js
@@ -2,20 +2,20 @@ let p = document.querySelector("p")
 let input = document.querySelector("input")
 
 const contacts = [
-    { name: "Thiago", number: "(32) 99999-9999" },
-    { name: "Michelly", number: "(32) 99999-9999" },
-    { name: "Liz", number: "(32) 99999-9999" },
-    { name: "Beatriz", number: "(32) 99999-9999" },
-    { name: "Luna Maria", number: "(32) 99999-9999" },
-    { name: "Maria", number: "(32) 99999-9999" },
-    { name: "Joana", number: "(32) 99999-9999" },
-    { name: "Pedro", number: "(32) 99999-9999" },
+    { name: "Thiago", number: "(32) 99999-9991" },
+    { name: "Michelly", number: "(32) 99999-9992" },
+    { name: "Liz", number: "(32) 99999-9993" },
+    { name: "Beatriz", number: "(32) 99999-9994" },
+    { name: "Luna Maria", number: "(32) 99999-9995" },
+    { name: "Maria", number: "(32) 99999-9996" },
+    { name: "Joana", number: "(32) 99999-9997" },
+    { name: "Pedro", number: "(32) 99999-9998" },
     { name: "Marisia", number: "(32) 99999-9999" },
-    { name: "Heitor", number: "(32) 99999-9999" },
-    { name: "Cacilda", number: "(32) 99999-9999" },
-    { name: "Carminha", number: "(32) 99999-9999" },
-    { name: "Weliton", number: "(32) 99999-9999" },
-    { name: "Elizi", number: "(32) 99999-9999" },
+    { name: "Heitor", number: "(32) 99999-9910" },
+    { name: "Cacilda", number: "(32) 99999-9911" },
+    { name: "Carminha", number: "(32) 99999-9912" },
+    { name: "Weliton", number: "(32) 99999-9913" },
+    { name: "Elizi", number: "(32) 99999-9914" },
 ]
 
 function search() {
